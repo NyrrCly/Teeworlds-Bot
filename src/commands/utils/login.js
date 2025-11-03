ee02e8f5723e5891cd9535e600145a9a6d4a7084
Filoqcus <135884105+Filoqcus@users.noreply.github.com>
2025-11-03 22:53:07 +0200

    Added: Logged In message

--- src/commands/utils/login.js
+++ src/commands/utils/login.js
@@ -1,8 +1,10 @@
 import LoginUtils from "../../core/utils/login.js";
+import Chat from "../../core/utils/chat.js";
 
 export default class LoginCommands {
     static async loginCommand(client, password, message) {
         if (client._LoginPassword === parseInt(password)) {
+            await Chat.sendMessage(client, `/w ${message.author.ClientInfo.name} You are logged in ♥`)
             client._LoggedClients.push(message.author.ClientInfo.name);
         }
         await LoginUtils.generateLoginCode(client)
