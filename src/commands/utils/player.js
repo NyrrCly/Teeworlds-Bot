6abea81a9fe32c6782277bc8d360a6973deab870
Filoqcus <135884105+Filoqcus@users.noreply.github.com>
2025-11-03 23:27:29 +0200

    Fixed: bug because "nameless tee"

--- src/commands/utils/player.js
+++ src/commands/utils/player.js
@@ -4,9 +4,19 @@ import DDnetUtils from "../../core/utils/ddnet/ddnetUtils.js";
 
 export default class PlayerCommands {
     static async sendPlayerInfo(client, nickname) {
-        const player = await DDNetPlayerRequest.getPlayerDDStatsJson(nickname)
+        const player = await DDNetPlayerRequest.getPlayerDDStatsJson(nickname);
         if (!player) return await Chat.sendMessage(client, `Unknown player name`);
-        const playerHours = await DDnetUtils.playerHours(player.general_activity.total_seconds_played)
-        return await Chat.sendMessage(client, `${player.profile.name} profile: points: ${player.profile.points} | hours: ${playerHours}`);
+
+        const totalSecondsPlayed = player?.general_activity?.total_seconds_played;
+        const playerHours = totalSecondsPlayed
+            ? await DDnetUtils.playerHours(totalSecondsPlayed)
+            : "Cn​H2n+1​OH"; //moonshine rofl
+
+        const name = player.profile.name;
+        const points = player.profile.points;
+
+        return await Chat.sendMessage(
+            client, `${name} profile: points: ${points} | hours: ${playerHours}`
+        );
     }
 }
